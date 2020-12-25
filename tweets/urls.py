8856a2c86a7854b4f29dc09e76d94524df874111
Gagan <gagandureja675@gmail.com>
2020-12-25 23:31:25 +0530

    create tweet added

--- tweets/urls.py
+++ tweets/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
 	path('', views.home_view, name='home_view'),
+	path('create-tweet', views.tweet_create_view, name='tweet_create_view'),
+
 	path('<int:tweet_id>', views.tweet_detail_view),
 
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
-from .models import tweet
+from .models import Tweet
+
+from .forms import TweetForm
+
 # Create your views here.
 
 
@@ -15,9 +18,16 @@ def home_view(request, *args, **kwargs):
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
 	try:
-		obj = tweet.objects.get(id=tweet_id)
+		obj = Tweet.objects.get(id=tweet_id)
 	except:
 		raise Http404
 	return HttpResponse(f'<h1>Hello {tweet_id} - {obj.content}</h1>')
 	
 
+def tweet_create_view(request, *args, **kwargs):
+	form = TweetForm(request.POST or None)
+	if form.is_valid():
+		obj= form.save(commit=False)
+		obj.save()
+		form = TweetForm()
+	return render(request, 'components/form.html', context={'form':form})
